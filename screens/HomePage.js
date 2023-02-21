c9f72a69cdeeac2a6ee9822ab6111a3406baaaef
minaz99 <hanymina43@gmail.com>
2023-02-21 05:18:11 +0300

    Set fotter of Homeoage

--- screens/HomePage.js
+++ screens/HomePage.js
@@ -1,16 +1,46 @@
 import { useNavigation } from "@react-navigation/native";
+import { useFonts } from "expo-font";
 import React, { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 function HomePage() {
+  const [fontsLoaded] = useFonts({
+    Ultra: require("../assets/fonts/AbrilFatface-Regular.ttf"),
+    Ultra2: require("../assets/fonts/LilitaOne-Regular.ttf"),
+  });
   const navigation = useNavigation();
   useLayoutEffect(() => {
     navigation.setOptions({ headerShown: false });
   });
   return (
-    <SafeAreaView>
-      <Text>HomePage</Text>
+    <SafeAreaView className="h-full p-2  bg-blue-200">
+      <View className="flex-col h-full">
+        <View className="flex-grow"></View>
+        <View className="rounded-lg  bg-white   p-2 space-x-4 flex-row">
+          <TouchableOpacity className="mx-auto">
+            <Text
+              className="text-xl   text-blue-200 "
+              style={{ fontFamily: "Ultra" }}
+            >
+              Requests
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="mx-auto">
+            <Text
+              className="text-xl   border-white "
+              style={{ fontFamily: "Ultra" }}
+            >
+              Offers
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="mx-auto">
+            <Text className="text-xl " style={{ fontFamily: "Ultra" }}>
+              Account
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
