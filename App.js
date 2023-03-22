d922abc2db13ef49efeaaab5baa4e825151569d0
minaz99 <hanymina43@gmail.com>
2023-03-22 19:32:58 +0100

    Added provider store

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
 import Categories from "./screens/Categories";
 import HomePage from "./screens/HomePage";
 import Login from "./screens/Login";
@@ -9,19 +10,22 @@ import MyRequests from "./screens/MyRequests";
 import Offers from "./screens/Offers";
 import Register from "./screens/Register";
 import WelcomePage from "./screens/WelcomePage";
+import store from "./store";
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="WelcomePage">
-        <Stack.Screen name="WelcomePage" component={WelcomePage} />
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="HomePage" component={HomePage} />
-        <Stack.Screen name="Offers" component={Offers} />
-        <Stack.Screen name="Categories" component={Categories} />
-        <Stack.Screen name="MyRequests" component={MyRequests} />
-      </Stack.Navigator>
+      <Provider store={store}>
+        <Stack.Navigator initialRouteName="WelcomePage">
+          <Stack.Screen name="WelcomePage" component={WelcomePage} />
+          <Stack.Screen name="Register" component={Register} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="HomePage" component={HomePage} />
+          <Stack.Screen name="Offers" component={Offers} />
+          <Stack.Screen name="Categories" component={Categories} />
+          <Stack.Screen name="MyRequests" component={MyRequests} />
+        </Stack.Navigator>
+      </Provider>
     </NavigationContainer>
   );
 }
