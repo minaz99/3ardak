ae9b6e0cf7ca6f9ba809113b6cfa216714ab306b
minaz99 <hanymina43@gmail.com>
2023-04-04 19:56:52 +0200

    Changed request component design and changed img

--- components/Requests Wrappers/Request.js
+++ components/Requests Wrappers/Request.js
@@ -9,38 +9,38 @@ import {
 
 const Request = (props) => {
   return (
-    <TouchableOpacity className="my-2 rounded-3xl ">
+    <TouchableOpacity className="my-2 rounded-3xl " id={props.id}>
       <ImageBackground
-        source={require("../../src/5317656.jpg")}
+        source={require("../../src/kl_14.jpg")}
         resizeMode="cover"
         borderRadius={15}
         className="p-2 space-y-2"
       >
         <View className="flex-row space-x-2 items-center ">
-          <UserCircleIcon className="w-16 h-16 " color={"black"} />
+          <UserCircleIcon className="w-16 h-16 " color={"white"} />
           <View className=""></View>
-          <Text className="text-black  ">{props.name}</Text>
+          <Text className="text-white  ">{props.name}</Text>
         </View>
         <View className="flex-row space-x-2 items-center">
-          <TagIcon className="w-16 h-16" color={"black"} />
+          <TagIcon className="w-16 h-16" color={"white"} />
           <View className="border-r-2 border-white"></View>
-          <Text className="text-black text-sm ">{props.category}</Text>
+          <Text className="text-white text-sm ">{props.category}</Text>
         </View>
         <View className="flex-row space-x-2 items-center">
-          <PhoneIcon className="w-16 h-16" color={"black"} />
+          <PhoneIcon className="w-16 h-16" color={"white"} />
           <View className="border-r-2 border-white"></View>
-          <Text className="text-black text-sm ">{props.phonenumber}</Text>
+          <Text className="text-white text-sm ">{props.title}</Text>
         </View>
         <View className="flex-row space-x-2 items-center">
-          <InboxStackIcon className="w-16 h-16" color={"black"} />
+          <InboxStackIcon className="w-16 h-16" color={"white"} />
           <View className="border-r-2 border-white"></View>
-          <Text className="text-black text-sm flex-shrink overflow-y-scroll">
+          <Text className="text-white text-sm flex-shrink overflow-y-scroll">
             {props.request}
           </Text>
         </View>
         <View className="">
-          <TouchableOpacity className="p-2  bg-black rounded-lg text-sm ">
-            <Text className="font-bold w-full text-center text-white">
+          <TouchableOpacity className="p-2  bg-white rounded-lg text-sm ">
+            <Text className="font-bold w-full text-center text-black">
               Make offer
             </Text>
           </TouchableOpacity>
