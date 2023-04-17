86dbac1005bdfdef8253abb7b2c5db6bebdd87bb
minaz99 <hanymina43@gmail.com>
2023-04-18 00:33:25 +0200

    Added my requests slice to the store

--- store.js
+++ store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import RegisterUserSlice from "./API calls/RegisterUserSlice";
 import LoginUserSlice from "./API calls/LoginUserSlice";
 import CategoriesRequestsSlice from "./API calls/Requests/CategoriesRequestsSlice";
+import MyRequestsSlice from "./API calls/Requests/MyRequestsSlice";
 export default configureStore({
   reducer: {
     registerUserSlice: RegisterUserSlice,
     login: LoginUserSlice,
     categoryRequests: CategoriesRequestsSlice,
+    myRequests: MyRequestsSlice,
   },
 });
