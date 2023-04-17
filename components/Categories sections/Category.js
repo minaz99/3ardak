97b3748a9b85b269bfc45391f2c29ba24b0ba1a7
minaz99 <hanymina43@gmail.com>
2023-04-18 00:31:10 +0200

    Changed categories colors to sky-600

--- components/Categories sections/Category.js
+++ components/Categories sections/Category.js
@@ -19,11 +19,11 @@ const Category = (props) => {
   };
 
   return (
-    <View className="p-4 border-b bg-white border-gray-300 flex-col">
-      <View className="flex-row items-center  ">
+    <View className="p-4 border-b bg-white  border-gray-300 flex-col">
+      <View className="flex-row items-center ">
         <View className="flex-1 flex-row space-x-1 items-center  ">
           <TouchableOpacity onPress={clickedCategory} className="">
-            <Text className="text-blue-400">{props.categoryName}</Text>
+            <Text className="text-sky-600">{props.categoryName}</Text>
           </TouchableOpacity>
           <TouchableOpacity
             onPress={() => setExpanded(!expaneded)}
@@ -32,12 +32,12 @@ const Category = (props) => {
             <ChevronDownIcon height="16" color={"#475569"} />
           </TouchableOpacity>
         </View>
-        <View className="rounded-full h-7 w-7 justify-center  bg-blue-200 ">
-          <Text className="text-center text-black">{props.requestsCount}</Text>
+        <View className="rounded-full h-7 w-7 justify-center  bg-sky-600 ">
+          <Text className="text-center text-white">{props.requestsCount}</Text>
         </View>
       </View>
       <View>
-        <Text className="text-sm text-gray-400">
+        <Text className="text-xs text-gray-400">
           {props.categoryDescription}
         </Text>
       </View>
