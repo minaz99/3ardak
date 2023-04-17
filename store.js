89d8aca71aa7a9dfce0b3db4a8c1305f61c24d01
minaz99 <hanymina43@gmail.com>
2023-04-18 00:42:33 +0200

    Added make offer slice to store

--- store.js
+++ store.js
@@ -3,11 +3,13 @@ import RegisterUserSlice from "./API calls/RegisterUserSlice";
 import LoginUserSlice from "./API calls/LoginUserSlice";
 import CategoriesRequestsSlice from "./API calls/Requests/CategoriesRequestsSlice";
 import MyRequestsSlice from "./API calls/Requests/MyRequestsSlice";
+import MakeOfferSlice from "./API calls/Offers/MakeOfferSlice";
 export default configureStore({
   reducer: {
     registerUserSlice: RegisterUserSlice,
     login: LoginUserSlice,
     categoryRequests: CategoriesRequestsSlice,
     myRequests: MyRequestsSlice,
+    makeOffer: MakeOfferSlice,
   },
 });
