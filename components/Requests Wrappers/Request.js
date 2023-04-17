c395c80cd5395a01e38253db9218e073942037d5
minaz99 <hanymina43@gmail.com>
2023-04-18 00:36:37 +0200

    reqyest hides "make offer" btn based on showOffer

--- components/Requests Wrappers/Request.js
+++ components/Requests Wrappers/Request.js
@@ -5,13 +5,17 @@ import {
   InboxStackIcon,
   TagIcon,
   PhoneIcon,
+  DocumentTextIcon,
+  InformationCircleIcon,
+  HashtagIcon,
 } from "react-native-heroicons/solid";
+import MakeOffer from "../Offers Wrappers/MakeOffer";
 
 const Request = (props) => {
   return (
-    <TouchableOpacity className="my-2 rounded-3xl " id={props.id}>
+    <TouchableOpacity className="my-2 rounded-3xl " key={props.id}>
       <ImageBackground
-        source={require("../../src/kl_14.jpg")}
+        source={require("../../src/indigo-blue-plain-textured-background-fabric-block-prints.jpg")}
         resizeMode="cover"
         borderRadius={15}
         className="p-2 space-y-2"
@@ -22,29 +26,48 @@ const Request = (props) => {
           <Text className="text-white  ">{props.name}</Text>
         </View>
         <View className="flex-row space-x-2 items-center">
-          <TagIcon className="w-16 h-16" color={"white"} />
+          <InboxStackIcon className="w-16 h-16" color={"white"} />
           <View className="border-r-2 border-white"></View>
           <Text className="text-white text-sm ">{props.category}</Text>
         </View>
         <View className="flex-row space-x-2 items-center">
-          <PhoneIcon className="w-16 h-16" color={"white"} />
+          <InformationCircleIcon className="w-16 h-16" color={"white"} />
           <View className="border-r-2 border-white"></View>
-          <Text className="text-white text-sm ">{props.title}</Text>
+          <Text className="text-white text-sm flex-shrink  ">
+            {props.title}
+          </Text>
         </View>
+        {/*
         <View className="flex-row space-x-2 items-center">
-          <InboxStackIcon className="w-16 h-16" color={"white"} />
+          <DocumentTextIcon className="w-16 h-16" color={"white"} />
           <View className="border-r-2 border-white"></View>
           <Text className="text-white text-sm flex-shrink overflow-y-scroll">
             {props.request}
           </Text>
-        </View>
-        <View className="">
-          <TouchableOpacity className="p-2  bg-white rounded-lg text-sm ">
-            <Text className="font-bold w-full text-center text-black">
-              Make offer
-            </Text>
-          </TouchableOpacity>
-        </View>
+  </View>*/}
+
+        {!props.showMakeOffer ? (
+          <View className="">
+            <TouchableOpacity
+              onPress={() =>
+                props.makeRequestObject(
+                  props.id,
+                  props.name,
+                  props.title,
+                  props.request,
+                  props.category
+                )
+              }
+              className="p-2  bg-white rounded-lg text-sm "
+            >
+              <Text className="font-bold w-full text-center text-black">
+                Make offer
+              </Text>
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <View></View>
+        )}
       </ImageBackground>
     </TouchableOpacity>
   );
