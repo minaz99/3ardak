f18ee2389dbe9f41fe85a2db49f535c6edb1537d
minaz99 <hanymina43@gmail.com>
2023-04-20 00:38:06 +0200

    Wrapper for my offer

---
+++ components/Offers Wrappers/MyOfferWrapper.js
@@ -0,0 +1,49 @@
+import { View, Text, ImageBackground } from "react-native";
+import React from "react";
+import {
+  CheckCircleIcon,
+  ClockIcon,
+  InboxStackIcon,
+  InformationCircleIcon,
+  XCircleIcon,
+} from "react-native-heroicons/solid";
+
+const MyOfferWrapper = (props) => {
+  const StatusIcon =
+    props.status === "FINISHED"
+      ? CheckCircleIcon
+      : props.status === "PENDING"
+      ? ClockIcon
+      : XCircleIcon;
+  const color =
+    props.status === "FINISHED"
+      ? "#16a34a"
+      : props.status === "PENDING"
+      ? "#9ca3af"
+      : "#dc2626";
+  return (
+    <View key={props.id}>
+      <ImageBackground
+        source={require("../../src/4.jpg")}
+        resizeMode="cover"
+        borderRadius={15}
+        className="p-2 space-y-2"
+      >
+        <View className="flex-row space-x-2 items-center">
+          <InformationCircleIcon className="w-16 h-16" color={"white"} />
+          <Text className="text-white text-sm flex-shrink overflow-y-scroll">
+            {props.description}
+          </Text>
+        </View>
+        <View className="flex-row space-x-2 items-center">
+          <StatusIcon className="w-16 h-16" color={color} />
+          <Text className="text-white text-sm flex-shrink overflow-y-scroll">
+            {props.status}
+          </Text>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default MyOfferWrapper;
