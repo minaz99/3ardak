bc3ce999ca5c235f436c45cc7d1ccfa6fa030343
minaz99 <hanymina43@gmail.com>
2023-04-20 00:40:05 +0200

    Added My offers screen

--- screens/Offers.js
+++ screens/Offers.js
@@ -1,18 +1,37 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import Footer from "../components/Footer";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
+import OffersRecieved from "../components/Offers Sections/OffersRecieved";
+import OffersSent from "../components/Offers Sections/OffersSent";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getMyOffers } from "../API calls/Offers/MyOffersSlice";
 
 const Offers = () => {
   const navigation = useNavigation();
+  /*const [activeView, setActiveView] = useState("offersReceived");
+  const offerRecievedColor =
+    activeView === "offersReceived" ? "#0284c7" : "#9ca3af";
+  const offerSentColor = activeView === "offersSent" ? "#0284c7" : "#9ca3af";*/
   useLayoutEffect(() => {
     navigation.setOptions({ headerShown: false });
   });
+  const dispatch = useDispatch();
+  const { myOffers } = useSelector((store) => store.myOffers);
+  const { token, id } = useSelector((store) => store.login);
+  useEffect(() => {
+    dispatch(getMyOffers({ token, id }));
+  }, []);
   return (
     <View>
       <SafeAreaView className="h-full bg-white">
-        <ScrollView horizontal={false} className="flex-grow"></ScrollView>
+        <View className="mx-auto pt-2 space-x-4 flex-row">
+          <Text className="text-gray-400  text-xl">My offers</Text>
+        </View>
+        <OffersSent myOffers={myOffers} />
         <View className="p-2">
           <Footer isActive="offers" />
         </View>
