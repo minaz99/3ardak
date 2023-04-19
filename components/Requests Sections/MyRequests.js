0bbc5b854b464129d76a089ff2641a6b3637d36e
minaz99 <hanymina43@gmail.com>
2023-04-20 00:38:32 +0200

    Added props to MyRequest component "offers count"

--- components/Requests Sections/MyRequests.js
+++ components/Requests Sections/MyRequests.js
@@ -19,6 +19,7 @@ const Myrequests = (props) => {
             category={req.category}
             status={req.status}
             request={req.title}
+            offersCount = {req.offersCount}
           />
         );
       })}
