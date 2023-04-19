c162eb0250ba19865628330441b37cb308634c7d
minaz99 <hanymina43@gmail.com>
2023-04-20 00:39:38 +0200

    Centered make offer + made it on top footer & reqs

--- screens/Requests.js
+++ screens/Requests.js
@@ -6,7 +6,7 @@ import { useState } from "react";
 import { useFonts } from "expo-font";
 import { useLayoutEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { ScrollView, View } from "react-native";
+import { ImageBackground, ScrollView, View } from "react-native";
 import MakeOffer from "../components/Offers Wrappers/MakeOffer";
 import { useEffect } from "react";
 
@@ -44,41 +44,40 @@ function Requests() {
   };
 
   return (
-    <SafeAreaView className="h-full  bg-white">
-      <View className="h-full ">
-        <View className="items-center flex-grow relative">
-          <ScrollView
-            horizontal={false}
-            className="flex-grow z-10 divide-y-2 divide-red-300 "
-            overScrollMode="never"
-          >
-            {requests.map((req) => {
-              return (
-                <Request
-                  id={req.id}
-                  name={req.requesteeName}
-                  title={req.title}
-                  request={req.description}
-                  category={req.category}
-                  makeRequestObject={makeRequestObject}
-                  showMakeOffer={showMakeOffer}
-                />
-              );
-            })}
-          </ScrollView>
-
-          {showMakeOffer === true ? (
-            <View className="z-10 p-6 h-full w-full  absolute ">
-              <MakeOffer
-                setShowMakeOffer={setShowMakeOffer}
-                reqObject={reqObj}
+    <SafeAreaView className="h-full bg-white">
+      <View className="flex-grow ">
+        <ScrollView
+          horizontal={false}
+          className="flex-grow z-10 p-2 divide-y-2 divide-red-300 "
+          overScrollMode="never"
+        >
+          {requests.map((req) => {
+            return (
+              <Request
+                id={req.id}
+                name={req.requesteeName}
+                title={req.title}
+                request={req.description}
+                category={req.category}
+                makeRequestObject={makeRequestObject}
                 showMakeOffer={showMakeOffer}
               />
-            </View>
-          ) : (
-            <View></View>
-          )}
-        </View>
+            );
+          })}
+        </ScrollView>
+
+        {showMakeOffer === true ? (
+          <View className="z-10 p-6 h-full -my-2  absolute ">
+            <MakeOffer
+              setShowMakeOffer={setShowMakeOffer}
+              reqObject={reqObj}
+              showMakeOffer={showMakeOffer}
+            />
+          </View>
+        ) : (
+          <View></View>
+        )}
+
         <View className="z-0  p-2">
           <Footer isActive="categories" requests={true} />
         </View>
