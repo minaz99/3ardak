e6f82da47b0ab63e2d161d7c6709bd552fd8307c
minaz99 <hanymina43@gmail.com>
2023-04-20 02:29:56 +0200

    viewing Offers Recieved

---
+++ components/Offers Sections/OffersRecieved.js
@@ -0,0 +1,27 @@
+import { View, Text, ScrollView } from "react-native";
+import React from "react";
+import OfferRecieved from "../Offers Wrappers/OfferRecieved";
+
+const OffersRecieved = (props) => {
+  return (
+    <ScrollView
+      horizontal={false}
+      className="p-2 mb-16 "
+      overScrollMode="never"
+    >
+      {props.offers.map((offer) => {
+        return (
+          <OfferRecieved
+            description={offer.description}
+            status={offer.status}
+            id={offer.id}
+            offerPrice={offer.offerPrice}
+            offereeName={offer.offereeName}
+          />
+        );
+      })}
+    </ScrollView>
+  );
+};
+
+export default OffersRecieved;
