72b43ad7f2a8d3507ef67bf24a57644cfab9a90f
minaz99 <hanymina43@gmail.com>
2023-04-20 02:29:22 +0200

    Offers per request component

---
+++ components/Offers Sections/OffersPerRequest.js
@@ -0,0 +1,41 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getOffersPerRequest } from "../../API calls/Offers/OffersPerRequestSlice";
+import OffersRecieved from "./OffersRecieved";
+import { XCircleIcon } from "react-native-heroicons/solid";
+
+const OffersPerRequest = (props) => {
+  const dispatch = useDispatch();
+  const { offers, offersCount } = useSelector(
+    (store) => store.offersPerRequest
+  );
+  useEffect(() => {
+    dispatch(
+      getOffersPerRequest({
+        token: props.token,
+        id: props.id,
+        reqID: props.reqID,
+      })
+    );
+  }, []);
+  return (
+    <View className="bg-sky-100 flex-col rounded-xl flex-grow ">
+      <View className="items-center pb-2  p-2 flex-row">
+        <View className="flex-1 "></View>
+        <TouchableOpacity
+          className="pt-2"
+          onPress={() => props.setShowOffers(false)}
+        >
+          <XCircleIcon className="w-16 h-16" color={"black"} />
+        </TouchableOpacity>
+      </View>
+      <View className="-mt-4">
+        <OffersRecieved offers={offers} />
+      </View>
+    </View>
+  );
+};
+
+export default OffersPerRequest;
